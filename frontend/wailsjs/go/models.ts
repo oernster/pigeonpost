660f5376e0e52d9a9e0179fda7835b1b76c744b7
oernster <1481439+oernster@users.noreply.github.com>
2026-07-14 02:55:40 +0100

    chore(bindings): commit regenerated Wails models so builds stop dirtying the tree

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -988,72 +988,6 @@ export namespace main {
 		}
 	}
 	
-	export class SearchHitDTO {
-	    message: MessageDTO;
-	    snippet: string;
-
-	    static createFrom(source: any = {}) {
-	        return new SearchHitDTO(source);
-	    }
-
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.message = this.convertValues(source["message"], MessageDTO);
-	        this.snippet = source["snippet"];
-	    }
-
-		convertValues(a: any, classs: any, asMap: boolean = false): any {
-		    if (!a) {
-		        return a;
-		    }
-		    if (a.slice && a.map) {
-		        return (a as any[]).map(elem => this.convertValues(elem, classs));
-		    } else if ("object" === typeof a) {
-		        if (asMap) {
-		            for (const key of Object.keys(a)) {
-		                a[key] = new classs(a[key]);
-		            }
-		            return a;
-		        }
-		        return new classs(a);
-		    }
-		    return a;
-		}
-	}
-
-	export class SearchResultDTO {
-	    hits: SearchHitDTO[];
-	    degraded: boolean;
-
-	    static createFrom(source: any = {}) {
-	        return new SearchResultDTO(source);
-	    }
-
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.hits = this.convertValues(source["hits"], SearchHitDTO);
-	        this.degraded = source["degraded"];
-	    }
-
-		convertValues(a: any, classs: any, asMap: boolean = false): any {
-		    if (!a) {
-		        return a;
-		    }
-		    if (a.slice && a.map) {
-		        return (a as any[]).map(elem => this.convertValues(elem, classs));
-		    } else if ("object" === typeof a) {
-		        if (asMap) {
-		            for (const key of Object.keys(a)) {
-		                a[key] = new classs(a[key]);
-		            }
-		            return a;
-		        }
-		        return new classs(a);
-		    }
-		    return a;
-		}
-	}
-
 	export class OutboxItemDTO {
 	    id: string;
 	    accountId: string;
@@ -1125,6 +1059,70 @@ export namespace main {
 	        this.contains = source["contains"];
 	        this.action = source["action"];
 	    }
+	}
+	export class SearchHitDTO {
+	    message: MessageDTO;
+	    snippet: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new SearchHitDTO(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.message = this.convertValues(source["message"], MessageDTO);
+	        this.snippet = source["snippet"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class SearchResultDTO {
+	    hits: SearchHitDTO[];
+	    degraded: boolean;
+	
+	    static createFrom(source: any = {}) {
+	        return new SearchResultDTO(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.hits = this.convertValues(source["hits"], SearchHitDTO);
+	        this.degraded = source["degraded"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
 	}
 	export class TagDTO {
 	    id: string;
